896440845be299fc789a6e221f6f307dc68096a5
Chuehleo <122568562+Chuehleo@users.noreply.github.com>
2023-03-29 00:01:30 -0700

    Update NavbarItem.tsx

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 
 interface NavbarItemProps {
-    label:string;
+  label: string;
+  onClick?: () => void;
 }
 
-const NavbarItem: React.FC<NavbarItemProps> = ({
-    label
-}) => {
-    return (
-        <div className="text-white cursor-pointer hover:text-gray-300 transition">
-           {label} 
-        </div>
-    )
-}
+const NavbarItem: React.FC<NavbarItemProps> = ({ label, onClick }) => {
+  return (
+    <div
+      className="text-white cursor-pointer hover:text-gray-300 transition"
+      onClick={onClick}
+    >
+      {label}
+    </div>
+  );
+};
 
 export default NavbarItem;
